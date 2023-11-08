b2da8b18a2c235752e3ecb41f60e391ac932bf76
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-08 14:47:51 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useCallback } from "react"
 
 function App() {
 
   const [userInput, setUserInput] = useState("")
+  const [result, setResult] = useState(0)
 
   const [num1] = useState(4)
   const [num5] = useState(5)
 
-  const sum = () => num1 + num5
+  const sum = useCallback(() => num1 + num5)
+
+  // const buildArray = () => {
+
+  // }
 
   useEffect(() => {
     console.log(`New sum. Value: ${sum()}`);
+    setResult(sum())
   }, [sum])
+
   return (
     <main className="App">
       <div className="input">
-        <input type="text" 
-      placeholder="input"
-      value={userInput}
-        onChange={ e => setUserInput(e.target.value)}
-        required
-      />
+        <input type="text"
+          placeholder="input"
+          value={userInput}
+          onChange={e => setUserInput(e.target.value)}
+          required
+        />
       </div>
       <h1>OutPut: <code> {userInput || ".."}</code></h1>
     </main>
