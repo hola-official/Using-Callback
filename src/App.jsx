b178a71b7c505d60051fbe32027072a7e8b7628a
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-08 12:11:43 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import './App.css'
+import { useEffect, useState } from "react"
 
 function App() {
 
+  const [userInput, setUserInput] = useState("")
+
+  const [num1] = useState(4)
+  const [num5] = useState(5)
+
+  const sum = () => num1 + num5
+
+  useEffect(() => {
+    console.log(`New sum. Value: ${sum()}`);
+  }, [sum])
   return (
     <>
-      
+
     </>
   )
 }
